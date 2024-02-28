506e50e77735eb2c4bf55e3b77b9f87fc5dd34ba
fujiN10 <abramenkolev1@yandex.ru>
2024-02-28 19:31:58 +0400

    1

---
+++ index.py
@@ -0,0 +1,36 @@
+from flask import Flask, url_for, request, render_template, redirect
+import json
+from loginform import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {}
+    param['username'] = "Ученик Яндекс.Лицея"
+    param['title'] = 'Домашняя страница'
+    return render_template('index.html', **param)
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd.html', number=2)
+
+@app.route('/news')
+def news():
+    with open("sample.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('sample_json.html', news=news_list)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+if __name__ == '__main__':
+
+    app.run(port=8080, host='127.0.0.1')
